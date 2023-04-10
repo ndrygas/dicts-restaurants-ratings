0817f4ac74a336d6a9cf9a3aab83c72dadc5f6fb
Nicolas Drygas <nicolas.drygas@live.com>
2023-04-10 14:06:13 -0700

    Completed all except last part

--- ratings.py
+++ ratings.py
@@ -1,4 +1,6 @@
 
+from random import choice 
+
 def read_file(input_file):
     """Restaurant rating lister."""
     ratings_dict = {}
@@ -28,12 +30,47 @@ def add_new_restaurant(dictionary):
     dictionary[restaurant_name] = rating
     return dict(sorted(dictionary.items()))
 
+def update_random_restaurant(dictionary):
+    random_restaurant= choice(list(dictionary.keys()))
+    print(f"{random_restaurant} is rated {dictionary[random_restaurant]}.")
+    while True:
+        new_rating = input("Please enter restaurant's rating: ")
+        if new_rating.isnumeric() == True:
+            new_rating = int(new_rating)
+            if new_rating >= 1 and new_rating <= 5:
+                break
+        print("The rating must be an integer between 1 and 5 (inclusive)")
+
+    dictionary[random_restaurant] = new_rating    
+
+    return dictionary
+
+
+def update_chosen_restaurant(dictionary):
+    chosen_restaurant= input("Please enter restaurant name you want to update: ")
+    if chosen_restaurant in dictionary:
+        print(f"{chosen_restaurant} is rated {dictionary[chosen_restaurant]}.")
+        while True:
+            new_rating = input("Please enter restaurant's rating: ")
+            if new_rating.isnumeric() == True:
+                new_rating = int(new_rating)
+                if new_rating >= 1 and new_rating <= 5:
+                    break
+            print("The rating must be an integer between 1 and 5 (inclusive)")
+
+        dictionary[chosen_restaurant] = new_rating
+    else: 
+        print("This restaurant does not exist")
+    return dictionary
+
 
 ratings_dict = read_file('scores.txt')
 while True:
     print("Menu")
     print("P - printing all the ratings (in alphabetical order)")
     print("A - Adding a new restaurant (and rating it)")
+    print("R - Update a random restaurant’s rating")
+    print("C- Update a chosen restaurant’s rating")
     print("Q - Quitting")
     user_input = input("Please choose an option: ").lower()
 
@@ -44,5 +81,9 @@ while True:
         print_ratings(ratings_dict)
     elif user_input == 'a':
         ratings_dict = add_new_restaurant(ratings_dict)
+    elif user_input == 'r':
+        ratings_dict = update_random_restaurant(ratings_dict)
+    elif user_input == 'c':
+        ratings_dict = update_chosen_restaurant(ratings_dict)
     else:
         print("Input not recognized, please try again.")
